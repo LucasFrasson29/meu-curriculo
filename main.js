3ae903f68fa88c276fe8e224a6f13e68c23a4dec
lucas frasson casagrande <lucas.f.casagrande29@gmail.com>
2025-12-27 12:09:21 -0300

    projeto

---
+++ main.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+}
+
+/* function toggleMode() {
+  const HTML = document.documentElement
+  HTML.classList.toggle("light")
+
+} */
