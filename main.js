cd301f9252ad7ca46aecfca703d03aeefecca364
lucas frasson casagrande <lucas.f.casagrande29@gmail.com>
2025-12-29 17:42:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,3 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 }
-
-/* function toggleMode() {
-  const HTML = document.documentElement
-  HTML.classList.toggle("light")
-
-} */
